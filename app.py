8cd6da6fb15a09ce55d5591be7780f3207c95ace
Alejandro Monasterio <amonasterio@gmail.com>
2022-11-29 10:35:59 +0100

    modificado para que coja también los valores de article:published_time

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from datetime import datetime
 import re
 from urllib.parse import unquote, urlparse
 from pathlib import  PurePosixPath
+import ptvsd
+ptvsd.enable_attach(address=('localhost', 5678))
+ptvsd.wait_for_attach() # Only include this line if you always want to attach the debugger
 
 #extrae el año y el mes, de una fecha con formaro YYYY-MM-DD 
 def extractYearMonth(str_fecha):
@@ -39,10 +42,14 @@ def convertDate(str_fecha):
             fecha=fecha[:5]+dia+"-"+mes
     return fecha
 
+
+
+
 #Añade el campo "date" con el formato YYYY-MM-DD y el campo "month" con el formato YYYY-MM al dataFrame
 def addFechaDataFrame(df):
-    df['date']=df.apply(lambda x: convertDate(x['datePublished 1']), 
-                        axis=1)
+   # df['date']=df.apply(lambda x: convertDate(x['datePublished 1']), 
+   #                     axis=1)
+    df['date'] = df.apply (lambda row: asignarFecha(row), axis=1)    
     df['month']=df.apply(lambda x: extractYearMonth(x['date']), 
                         axis=1)
     return df  
@@ -78,6 +85,15 @@ st.set_page_config(
 st.title("Obtener volumen de contenidos creados por mes")
 st.text("Este script recibe un fichero con datos exportados de ScreamingFrog\nEs imprescindible que contenga los campos Address, Indexability, datePublished 1 (resultado de extraer los valores del campo datePublished)\nSe filtrarán autimáticamente los resultados con valor Indexability=Indexable y datePublished 1 not null\nAdicionalmente podemos añadir que se filtres únicamente aquellas URL (Address) que contengan cierto texto (con el objetivo de eliminar URL que no sean posts)\nFinalmente devolverá el número de posts publicados en cada mes\nAdemás, se pueden agrupar los resultados por pahts (en el caso de que la URL contenga la categoría como path) indicando la profundidad del path que se quiera extraer")
 
+#Obtiene la fecha de datePublished 1 o published_time 1 con el formato YYYY-MM-DD
+def asignarFecha(row):
+    fecha=''
+    if row['datePublished 1']==None:
+       fecha= convertDate(row['published_time 1'])
+    else:
+       fecha= convertDate(row['datePublished 1'])
+    return fecha
+
 uploaded_file = st.file_uploader("Fichero con el listado de URL", type='csv')
 if uploaded_file is not None:
     df_entrada = pd.read_csv(uploaded_file)
@@ -85,10 +101,10 @@ if uploaded_file is not None:
     #Si además, debemos dejar los que contengan ciertos caracteres en las URL
     filtro_url=st.text_input("Introduzca el texto por el que debemos filtar las URL en caso de ser necesario","")
     df_entrada=filtraResultados(df_entrada,filtro_url)
-    st.dataframe(df_entrada[["Address","datePublished 1"]])
+    st.dataframe(df_entrada[["Address","datePublished 1", "published_time 1"]])
     #Si tras filtrar no tenemos un dataframe vacío continuamos
     if not df_entrada.empty:
-        #Añadimos la columna "date" con la fecha en el formato que queremos, y la columna "month" con el formato YYYY-MM 
+        ##Añadimos la columna "date" con la fecha en el formato que queremos, y la columna "month" con el formato YYYY-MM 
         df_nuevo=addFechaDataFrame(df_entrada)
         #Creamos dataframe con datos agrupados por mes
         df_agrupado=df_nuevo.groupby(['month'],as_index=False).agg(total=('month','size'))
